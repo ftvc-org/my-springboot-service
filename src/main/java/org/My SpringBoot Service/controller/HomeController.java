cad93c57b7ed4c01db5280457aa0d422ca7db07b
tejaltanajisawale <tejal.tanajisawale@fiserv.com>
2026-01-20 11:03:51 +0530

    Update HomeController.java

--- src/main/java/org/My SpringBoot Service/controller/HomeController.java
+++ src/main/java/org/My SpringBoot Service/controller/HomeController.java
@@ -1,4 +1,4 @@
-package org.My SpringBoot Service.controller;
+package org.my_springboot_service.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
