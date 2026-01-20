560901fee775c2bd403b83fc5a4285bb17c52237
tejaltanajisawale <tejal.tanajisawale@fiserv.com>
2026-01-20 11:02:10 +0530

    Update Application.java

--- src/main/java/org/My SpringBoot Service/Application.java
+++ src/main/java/org/My SpringBoot Service/Application.java
@@ -1,4 +1,4 @@
-package org.My SpringBoot Service;
+package org.my_springboot_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
